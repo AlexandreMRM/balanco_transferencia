2a5831d4c28b61a24ef87a474463bd1f29f04214
AlexandreMRM <67433372+AlexandreMRM@users.noreply.github.com>
2025-02-20 19:38:38 -0300

    Update Main_TRF_Mansear.py

--- Main_TRF_Mansear.py
+++ Main_TRF_Mansear.py
@@ -4,9 +4,7 @@ import Lancar_TRF_Mansear as Lancar_TRF_Mansear
 import pandas as pd
 from datetime import datetime
 import streamlit as st
-import xlwings as xl
-import tempfile
-import openpyxl
+import planilha_sheet
 
 
 def TRF_Mansear():
@@ -34,86 +32,53 @@ def TRF_Mansear():
 
     if 'df' not in st.session_state:
         st.session_state.df = None
+    if 'df_editado' not in st.session_state:
+        st.session_state.df_editado = None
 
-    Plan = st.file_uploader('Favor inserir a planilha do Excel', type='xlsx', key='inp_file_01')
 
-    if Plan is not None:
-        try:
-            xls = pd.ExcelFile(Plan)
-            Sheet = pd.read_excel(xls, sheet_name="Planilha1")
+    Data = st.date_input('Informe a Data da Transferencia', "today", format= "DD/MM/YYYY", key='inp_data_01')
+    Nome_Balanco = st.selectbox('Informe o Catamarã', ['MANSEAR 01', 'MANSEAR 02', 'MANSEAR 03', 'MANSEAR 04'], key='inp_nome_01')
 
-            Data = st.date_input('Informe a Data da Transferencia', "today", format= "DD/MM/YYYY", key='inp_data_01')
-            Nome_Balanco = st.selectbox('Informe o Catamarã', ['Mansear 01', 'Mansear 02', 'Mansear 03', 'Mansear 04'], key='inp_nome_01')
+    Data = Data.strftime('%d/%m/%Y')
 
-            Data = datetime.strftime(Data, '%d/%m/%Y')
-
-            if 'df' not in st.session_state:
-                st.session_state.df = None
-
-            if st.button('Carregar Dados da Planilha', key='inp_button_01'):
-                #xls = pd.ExcelFile(Plan)
-                df = pd.read_excel(xls, sheet_name="Planilha1", usecols=['Cod', 'Descricao', 'Quantidade', 'Status'], header=0)
-                
-                st.session_state.df = df
-                
-        except Exception as e:
-            st.error(f'Erro ao Carregar Planilha: {e}')
-    else:
-        st.warning('Anexe o arquivo da Transferencia')
+    if st.button('Carregar Dados da Planilha', key='inp_button_01'):
+        st.session_state.df = planilha_sheet.planilha()
 
     if st.session_state.df is not None:
         df_editado = st.data_editor(st.session_state.df, use_container_width=True, hide_index=True, key='editar_planilha')
-        if st.button('Lançar', key='inp_nome_04'):
-            df = df_editado
+        st.session_state.df_editado = df_editado
 
-            with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as temp_file:
-                #temp_file.write(Plan.getvalue())
-                temp_file_path = temp_file.name
-                with open(temp_file_path, 'wb') as f:
-                    f.write(Plan.getvalue())
-            
-            try:
-                #plan_usuario = xl.Book(temp_file_path)
-                plan_usuario = openpyxl.load_workbook(temp_file_path)
-                #Aba = plan_usuario.sheets[0]
-                Aba = plan_usuario.active
-                
-                status_placeholder = st.empty()
-                df_placeholder = st.empty()
+    if st.session_state.df_editado is not None and st.button('Lançar', key='inp_nome_06'):
+        df = st.session_state.df_editado
 
-                for i in range(len(df)):
-                    cod = str(df['Cod'][i])
-                    status_placeholder.write(f"Lançando {cod} - Descrição {df['Descricao'][i]}...")
-                    CodOmie = Produtos.Prod(cod)
+        status_placeholder = st.empty()
+        df_placeholder = st.empty()
 
-                    if CodOmie == None:
-                        st.warning(f"Produto {df['Descricao'][i]} não encontrado no Omie")
-                        df.loc[i, 'Status'] = 'Erro'
-                        continue
-                    else:
-                        Qtde = str(df['Quantidade'][i])
-                        Valor = Preco.BuscaPreco(cod, Data)
-                        Resultado = Lancar_TRF_Mansear.Lancamento(CodOmie, Data, Qtde, Nome_Balanco, Valor)
-                        if Resultado:
-                            st.success(f"STATUS - OK {cod} - {df['Descricao'][i]} - {Valor}")
-                            df.loc[i, 'Status'] = 'Lançado'
+        for i in range(len(df)):
+            cod = str(df['Cod'][i])
+
+            status_placeholder.write(f"Lançando {cod} - Descrição {df['Descricao'][i]}...")
+            CodOmie = Produtos.Prod(cod)
+
+            if CodOmie == None:
+                st.warning(f"Produto {df['Descricao'][i]} não encontrado no Omie")
+                df.loc[i, 'Status'] = 'Erro'
+                continue
+            else:
+                Qtde = str(df['Quantidade'][i])
+                Valor = Preco.BuscaPreco(cod, Data)
+                Resultado = Lancar_TRF_Mansear.Lancamento(CodOmie, Data, Qtde, Nome_Balanco, Valor)
+                if Resultado:
+                    st.success(f"STATUS - OK {cod} - {df['Descricao'][i]} - {Valor}")
+                    df.loc[i, 'Status'] = 'Lançado'
                             
-                        else:
-                            st.warning(f"Lançamento Codigo - {cod} Falhou")
-                            df.loc[i, 'Status'] = 'Erro'
-                            #st.data_editor(df, use_container_width=True, hide_index=True, key="planilha_atualizada_erro")
-                    df_placeholder.dataframe(df, hide_index=True)
-                    #Aba.range("A2:D1000").clear_contents()
-                    #Aba.range("A2").value = df.to_numpy()
-                    for row_idx, row in enumerate(df.itertuples(index=False, name=None), start=2):
-                        for col_idx, value in enumerate(row, start=1):
-                            Aba.cell(row=row_idx, column=col_idx, value=value)
+                else:
+                    st.warning(f"Lançamento Codigo - {cod} Falhou")
+                    df.loc[i, 'Status'] = 'Erro'
+                    
+                df_placeholder.dataframe(df, hide_index=True, use_container_width=True)
+                planilha_sheet.salvar_planilha(df)
 
                 st.session_state.df = df
 
-                st.success("Processo Finalizado - VERIFICAR LANÇAMENTOS")
-            except Exception as e:
-                st.error(f'Erro {e} - Planilha não carregada')
-            
-
-
+        st.success("Processo Finalizado - VERIFICAR LANÇAMENTOS")
